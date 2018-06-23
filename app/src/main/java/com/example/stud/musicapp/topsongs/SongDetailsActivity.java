9fe00fa91e77935d038bc4af99f1eb963dec6ddd
Marcin Ochyra <mochyra@wsiz.rzeszow.pl>
2018-06-23 12:38:34 +0200

    Dodanie menu

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -82,5 +85,27 @@ public class SongDetailsActivity extends AppCompatActivity {
         onBackPressed();
 
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.favorites_menu, menu);
+        return true;
+    }
+
+    private void addRemoveFavorite() {
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemFavorite:
+                addRemoveFavorite();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
